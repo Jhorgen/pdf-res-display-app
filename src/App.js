1dfa7881a39020ddb033919cccf56f8273e35f23
Josh Horgen <39712421+Jhorgen@users.noreply.github.com>
2022-08-11 17:19:52 -0700

    Create repo.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SinglePage from './components/pdf-pages/single-page.js'; //check navigation path structure. for manual entry.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <SinglePage />
     </div>
   );
 }
