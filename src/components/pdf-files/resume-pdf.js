087f18feaf54e0521c36eb6e3b96aa057ba97a0b
Josh Horgen <39712421+Jhorgen@users.noreply.github.com>
2022-08-17 13:38:56 -0700

    Add state back to resume-pdf component.

--- src/components/pdf-files/resume-pdf.js
+++ src/components/pdf-files/resume-pdf.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 export default function ResPDF(props) {
 
-const { pdf } = props;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { pdf } = props;
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
 
   return (
     <div>
     <Document
       file={pdf}
       options={{  workerSrc: '/pdf.worker.js'  }}
+      onLoadSuccess={onDocumentLoadSuccess}
       >
-      <Page pageNumber={1} />
+      <Page pageNumber={pageNumber} />
     </Document>
     </div>
   );
